397bb089062edbad73784a7ff91872461c5f29bb
Ahmed Nashif <a.nashif7@gmail.com>
2023-11-10 19:28:45 +0600

    environement

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri = `mongodb+srv://${process.env.DB_SSUSER}:${process.env.DB_SSPASS}@cluster0.c1krwnm.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://digiUser:3Z6ReWkE5te1SVBc@cluster0.c1krwnm.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://digiUser:3Z6ReWkE5te1SVBc@cluster0.c1krwnm.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c1krwnm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
